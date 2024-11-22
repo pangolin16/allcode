8aed3c6f790158301e5589185c7c91306679dc7a
pangolin16 <m.tojnar@gmail.com>
2024-11-22 11:06:55 +0100

    Add files via upload

---
+++ calc/calc.js
@@ -0,0 +1,6 @@
+function funkcez(){var promena=document.getElementById("vstup").value
+    document.getElementById("vystup").innerHTML=parseInt(promena)/320*60;
+}
+// function funkcey(){var promena=document.getElementById("vstup").value
+//     document.getElementById("vystup").innerHTML=parseInt(promena)/1000*60;
+// }
