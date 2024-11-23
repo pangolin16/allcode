323e3978c7e44de72674fe44d3d6cb187da6e8be
pangolin <m.tojnar@gmail.com>
2024-11-23 11:58:19 +0100

    remove calc

--- calc/calc.js
+++
@@ -1,6 +0,0 @@
-function funkcez(){var promena=document.getElementById("vstup").value
-    document.getElementById("vystup").innerHTML=parseInt(promena)/320*60;
-}
-// function funkcey(){var promena=document.getElementById("vstup").value
-//     document.getElementById("vystup").innerHTML=parseInt(promena)/1000*60;
-// }
