c24c1918cd1294916c8acecd0daa2061ccf24faa
pangolin <m.tojnar@gmail.com>
2024-11-24 19:12:32 +0100

    icons change

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -460,18 +460,3 @@ function convertTime(time) {
 
 
 
-nextBtn.addEventListener("click", gotoDate);
-
-function gotoDate() {
-
-  const dateArr = dateInput.value.split("/");
-  if (dateArr.length === 2) {
-    if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
-      month = dateArr[0] - 1;
-      year = dateArr[1];
-      initCalendar();
-      return;
-    }
-  }
-  alert("Invalid Date");
-}
