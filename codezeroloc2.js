f0bd317f0f7f561a0f83f8542ceecdcf3c276113
pangolin <m.tojnar@gmail.com>
2025-03-27 17:50:33 +0100

    comments add

---
+++ codezeroloc2.js
@@ -0,0 +1,186 @@
+function zmena() {
+    var image = document.getElementById('obraz');
+    if (image.src.match("images/separated.svg")) {
+      image.src = "images/whole.svg";
+    } else {
+      image.src = "images/separated.svg";
+    }
+  }
+function gets1(){var image = document.getElementById('obraz');
+    if (image.src.match("separated")) {gets1a();}
+   else {gets1b();}}
+
+
+function gets1a(){
+     let s1=document.getElementById("s1").value
+
+     if(s1<=150&& s1>10){document.getElementById("output1").innerHTML=parseInt(s1)+10}
+     else if(s1<=250 && s1>150){document.getElementById("output1").innerHTML=parseInt(s1)+11}
+     else if(s1<=400&& s1>250){document.getElementById("output1").innerHTML=parseInt(s1)+12}
+     else if(s1<=450 && s1>400){document.getElementById("output1").innerHTML=parseInt(s1)+13}
+     else if(s1<=650 && s1>450){document.getElementById("output1").innerHTML=parseInt(s1)+14}
+     else if(s1<=950 && s1>650){document.getElementById("output1").innerHTML=parseInt(s1)+15}
+ else if(s1<=1300 && s1>950){document.getElementById("output1").innerHTML=parseInt(s1)+16}
+ else if(s1<=1750 && s1>1300){document.getElementById("output1").innerHTML=parseInt(s1)+17}
+ else if(s1<=1800 && s1>1750){document.getElementById("output1").innerHTML=parseInt(s1)+18}
+ else if(s1<=1900 && s1>1800){document.getElementById("output1").innerHTML=parseInt(s1)+19}
+ else if(s1<=2000&& s1>1900){document.getElementById("output1").innerHTML=parseInt(s1)+20}
+ else if(s1<=2500 && s1>2000){document.getElementById("output1").innerHTML=parseInt(s1)+21}
+ else if(s1>9999){document.getElementById("output1").innerHTML="#"}
+ else if(s1<10){document.getElementById("output1").innerHTML="#"}
+ else{document.getElementById("output1").innerHTML=Math.round(parseInt(s1)+(Math.log(parseInt(s1))*5.756+22.2139))
+  }}
+
+  function gets1b(){let s1=document.getElementById("s1").value
+    if(s1<150){document.getElementById("output1").innerHTML=parseInt(s1)-18
+    }else if(s1<=250 && s1>150){document.getElementById("output1").innerHTML=parseInt(s1)-18}
+    else if(s1<=300&& s1>250){document.getElementById("output1").innerHTML=parseInt(s1)-19}
+    else if(s1<=500 && s1>300){document.getElementById("output1").innerHTML=parseInt(s1)-19}
+    else if(s1<=650 && s1>500){document.getElementById("output1").innerHTML=parseInt(s1)-19}
+    else if(s1<=750 && s1>650){document.getElementById("output1").innerHTML=parseInt(s1)-19}
+ else if(s1<=1300 && s1>750){document.getElementById("output1").innerHTML=parseInt(s1)-19}
+ else if(s1<=1500 && s1>1300){document.getElementById("output1").innerHTML=parseInt(s1)-19}
+ else if(s1<=1650 && s1>1500){document.getElementById("output1").innerHTML=parseInt(s1)-19}
+ else if(s1<=800 && s1>1650){document.getElementById("output1").innerHTML=parseInt(s1)-19}
+ else if(s1<=2000&& s1>1800){document.getElementById("output1").innerHTML=parseInt(s1)-19}
+ else if(s1<=2500 && s1>2000){document.getElementById("output1").innerHTML=parseInt(s1)-19}
+ else if(s1>9999){document.getElementById("output1").innerHTML="#"}
+ else if(s1<10){document.getElementById("output1").innerHTML="#"}
+ else{document.getElementById("output1").innerHTML=parseInt(s1)-19}}
+ 
+
+
+
+
+
+ function gets2(){var image = document.getElementById('obraz');
+    if (image.src.match("separated")) {gets2a();}
+   else {gets2b();}}
+
+
+
+    function gets2a(){let s2=document.getElementById("s2").value
+        if(s2<150){document.getElementById("output2").innerHTML=parseInt(s2)
+        }else if(s2<=250 && s2>150){document.getElementById("output2").innerHTML=parseInt(s2)+1}
+        else if(s2<=300&& s2>250){document.getElementById("output2").innerHTML=parseInt(s2)+2}
+        else if(s2<=500 && s2>300){document.getElementById("output2").innerHTML=parseInt(s2)+3}
+        else if(s2<=650 && s2>500){document.getElementById("output2").innerHTML=parseInt(s2)+4}
+        else if(s2<=950 && s2>650){document.getElementById("output2").innerHTML=parseInt(s2)+5}
+    else if(s2<=1300 && s2>950){document.getElementById("output2").innerHTML=parseInt(s2)+6}
+    else if(s2<=1500 && s2>1300){document.getElementById("output2").innerHTML=parseInt(s2)+7}
+    else if(s2<=1650 && s2>1500){document.getElementById("output2").innerHTML=parseInt(s2)+8}
+    else if(s2<=1800 && s2>1650){document.getElementById("output2").innerHTML=parseInt(s2)+9}
+    else if(s2<=2000&& s2>1800){document.getElementById("output2").innerHTML=parseInt(s2)+10}
+    else if(s2<=2500 && s2>2000){document.getElementById("output2").innerHTML=parseInt(s2)+11}
+    else if(s2>9999){document.getElementById("output2").innerHTML="#"}
+    else if(s2<10){document.getElementById("output2").innerHTML="#"}
+    else{document.getElementById("output2").innerHTML=Math.round(parseInt(s2)+(Math.log(parseInt(s2))*4.9755+32.7087))}}
+
+    function gets2b(){let s2=document.getElementById("s2").value
+        if(s2<150){document.getElementById("output2").innerHTML=parseInt(s2)
+        }else if(s2<=250 && s2>150){document.getElementById("output2").innerHTML=parseInt(s2)}
+        else if(s2<=300&& s2>250){document.getElementById("output2").innerHTML=parseInt(s2)}
+        else if(s2<=500 && s2>300){document.getElementById("output2").innerHTML=parseInt(s2)+1}
+        else if(s2<=650 && s2>500){document.getElementById("output2").innerHTML=parseInt(s2)+2}
+        else if(s2<=950 && s2>650){document.getElementById("output2").innerHTML=parseInt(s2)+2}
+    else if(s2<=1300 && s2>950){document.getElementById("output2").innerHTML=parseInt(s2)+2}
+    else if(s2<=1500 && s2>1300){document.getElementById("output2").innerHTML=parseInt(s2)+2}
+    else if(s2<=1650 && s2>1500){document.getElementById("output2").innerHTML=parseInt(s2)+2}
+    else if(s2<=1800 && s2>1650){document.getElementById("output2").innerHTML=parseInt(s2)+2}
+    else if(s2<=2000&& s2>1800){document.getElementById("output2").innerHTML=parseInt(s2)+2}
+    else if(s2<=2500 && s2>2000){document.getElementById("output2").innerHTML=parseInt(s2)+2}
+    else if(s2>9999){document.getElementById("output2").innerHTML="#"}
+    else if(s2<10){document.getElementById("output2").innerHTML="#"}
+    else{document.getElementById("output2").innerHTML=Math.round(parseInt(s2)+(Math.log(parseInt(s2))*4.9755+32.7087))}}
+    
+
+
+    function gets3(){var image = document.getElementById('obraz');
+        if (image.src.match("separated")) {gets3a();}
+       else {gets3b();}}
+    
+    
+
+
+
+    function gets3a(){let s3=document.getElementById("s3").value
+            if(s3<150){document.getElementById("output3").innerHTML=parseInt(s3)
+            }else if(s3<=250 && s3>150){document.getElementById("output3").innerHTML=parseInt(s3)+1}
+        else if(s3<=300&& s3>250){document.getElementById("output3").innerHTML=parseInt(s3)+2}
+        else if(s3<=500 && s3>300){document.getElementById("output3").innerHTML=parseInt(s3)+3}
+        else if(s3<=650 && s3>500){document.getElementById("output3").innerHTML=parseInt(s3)+4}
+        else if(s3<=950 && s3>650){document.getElementById("output3").innerHTML=parseInt(s3)+5}
+    else if(s3<=1300 && s3>950){document.getElementById("output3").innerHTML=parseInt(s3)+6}
+    else if(s3<=1500 && s3>1300){document.getElementById("output3").innerHTML=parseInt(s3)+7}
+    else if(s3<=1650 && s3>1500){document.getElementById("output3").innerHTML=parseInt(s3)+8}
+    else if(s3<=1800 && s3>1650){document.getElementById("output3").innerHTML=parseInt(s3)+9}
+    else if(s3<=2000&& s3>1800){document.getElementById("output3").innerHTML=parseInt(s3)+10}
+    else if(s3<=2500 && s3>2000){document.getElementById("output3").innerHTML=parseInt(s3)+11}
+    else if(s3>9999){document.getElementById("output3").innerHTML="#"}
+    else if(s3<10){document.getElementById("output3").innerHTML="#"}
+        else{document.getElementById("output3").innerHTML=Math.round(parseInt(s3)+(Math.log(parseInt(s3))*4.9755+32.7087))}}
+
+        function gets3b(){let s3=document.getElementById("s3").value
+            if(s3<150){document.getElementById("output3").innerHTML=parseInt(s3)
+            }
+        else if(s3<=250 && s3>150){document.getElementById("output3").innerHTML=parseInt(s3)}
+        else if(s3<=300&& s3>250){document.getElementById("output3").innerHTML=parseInt(s3)+1}
+        else if(s3<=500 && s3>300){document.getElementById("output3").innerHTML=parseInt(s3)+2}
+        else if(s3<=650 && s3>500){document.getElementById("output3").innerHTML=parseInt(s3)+2}
+        else if(s3<=950 && s3>650){document.getElementById("output3").innerHTML=parseInt(s3)+2}
+        else if(s3<=1300 && s3>950){document.getElementById("output3").innerHTML=parseInt(s3)+2}
+        else if(s3<=1500 && s3>1300){document.getElementById("output3").innerHTML=parseInt(s3)+2}
+        else if(s3<=1650 && s3>1500){document.getElementById("output3").innerHTML=parseInt(s3)+2}
+        else if(s3<=1800 && s3>1650){document.getElementById("output3").innerHTML=parseInt(s3)+2}
+        else if(s3<=2000&& s3>1800){document.getElementById("output3").innerHTML=parseInt(s3)+2}
+        else if(s3<=2500 && s3>2000){document.getElementById("output3").innerHTML=parseInt(s3)+2}
+        else if(s3>9999){document.getElementById("output3").innerHTML="#"}
+        else if(s3<10){document.getElementById("output3").innerHTML="#"}
+        else{document.getElementById("output3").innerHTML=Math.round(parseInt(s3)+(Math.log(parseInt(s3))*4.9755+32.7087))}}
+
+        function gets4(){var image = document.getElementById('obraz');
+            if (image.src.match("separated")) {gets4a();}
+           else {gets4b();}}
+        
+        
+
+
+            function gets4a(){let s4=document.getElementById("s4").value
+                if(s4<150){document.getElementById("output4").innerHTML=parseInt(s4)}
+            else if(s4<=250 && s4>150){document.getElementById("output4").innerHTML=parseInt(s4)+1}
+            else if(s4<=300&& s4>250){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+            else if(s4<=500 && s4>300){document.getElementById("output4").innerHTML=parseInt(s4)+3}
+            else if(s4<=650 && s4>500){document.getElementById("output4").innerHTML=parseInt(s4)+4}
+            else if(s4<=950 && s4>650){document.getElementById("output4").innerHTML=parseInt(s4)+5}
+        else if(s4<=1300 && s4>950){document.getElementById("output4").innerHTML=parseInt(s4)+6}
+        else if(s4<=1500 && s4>1300){document.getElementById("output4").innerHTML=parseInt(s4)+7}
+        else if(s4<=1650 && s4>1500){document.getElementById("output4").innerHTML=parseInt(s4)+8}
+        else if(s4<=1800 && s4>1650){document.getElementById("output4").innerHTML=parseInt(s4)+9}
+        else if(s4<=2000&& s4>1800){document.getElementById("output4").innerHTML=parseInt(s4)+10}
+        else if(s4<=2500 && s4>2000){document.getElementById("output4").innerHTML=parseInt(s4)+11}
+        else if(s4>9999){document.getElementById("output4").innerHTML="#"}
+        else if(s4<10){document.getElementById("output4").innerHTML="#"}
+            else{document.getElementById("output4").innerHTML=Math.round(parseInt(s4)+(Math.log(parseInt(s4))*4.9755+32.7087))}}
+
+
+
+
+
+    function gets4b(){let s4=document.getElementById("s4").value
+        if(s4<150){document.getElementById("output4").innerHTML=parseInt(s4)}
+    else if(s4<=250 && s4>150){document.getElementById("output4").innerHTML=parseInt(s4)+1}
+    else if(s4<=300&& s4>250){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+    else if(s4<=500 && s4>300){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+    else if(s4<=650 && s4>500){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+    else if(s4<=950 && s4>650){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+else if(s4<=1300 && s4>950){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+else if(s4<=1500 && s4>1300){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+else if(s4<=1650 && s4>1500){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+else if(s4<=1800 && s4>1650){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+else if(s4<=2000&& s4>1800){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+else if(s4<=2500 && s4>2000){document.getElementById("output4").innerHTML=parseInt(s4)+2}
+else if(s4>9999){document.getElementById("output4").innerHTML="#"}
+else if(s4<10){document.getElementById("output4").innerHTML="#"}
+    else{document.getElementById("output4").innerHTML=Math.round(parseInt(s4)+(Math.log(parseInt(s4))*4.9755+32.7087))}}
+
+
--- timer.js
+++ timer.js
@@ -1,60 +1,155 @@
-const el = document.querySelector(".clock");
-const bell = document.querySelector("audio");
+var validWords = [
+  "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", 
+  "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", 
+  "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", 
+  "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", 
+  "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", 
+  "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", 
+  "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", 
+  "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", 
+  "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", 
+  "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", 
+  "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", 
+  "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", 
+  "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", 
+  "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", 
+  "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", 
+  "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", 
+  "Korea, South", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", 
+  "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", 
+  "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", 
+  "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", 
+  "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", 
+  "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", 
+  "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", 
+  "Nicaragua", "Niger", "Nigeria", "North Macedonia", "Norway", 
+  "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", 
+  "Paraguay", "Peru", "Philippines", "Poland", "Portugal", 
+  "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", 
+  "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", 
+  "San Marino", "Sao Tome and Principe", "Saudi Arabia", 
+  "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", 
+  "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", 
+  "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", 
+  "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", 
+  "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", 
+  "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", 
+  "Ukraine", "United Arab Emirates", "United Kingdom", 
+  "United States", "Uruguay", "Uzbekistan", "Vanuatu", 
+  "Vatican City", "Venezuela", "Vietnam", "Yemen", 
+  "Zambia", "Zimbabwe"
+];
+var commonAnimalNames = [
+  "Dog", "Cat", "Fish", "Bird", "Horse", "Rabbit", "Hamster", "Turtle", 
+  "Lizard", "Snake", "Frog", "Guinea Pig", "Parrot", "Chicken", "Duck", 
+  "Goat", "Sheep", "Pig", "Cow", "Deer", "Fox", "Bear", "Wolf", "Lion", 
+  "Tiger", "Elephant", "Giraffe", "Zebra", "Monkey", "Kangaroo", "Panda", 
+  "Koala", "Otter", "Seal", "Whale", "Dolphin", "Octopus", "Crab", 
+  "Lobster", "Shrimp", "Ant", "Bee", "Butterfly", "Moth", "Spider", 
+  "Scorpion", "Dragonfly", "Grasshopper", "Cockroach", "Termite", 
+  "Slug", "Snail", "Bat", "Raccoon", "Squirrel", "Chipmunk", "Hedgehog", 
+  "Porcupine", "Bison", "Buffalo", "Cheetah", "Leopard", "Hyena", 
+  "Jaguar", "Rhino", "Hippo", "Camel", "Gorilla", "Chimpanzee", 
+  "Orangutan", "Sloth", "Anteater", "Armadillo", "Ocelot", "Serval", 
+  "Tasmanian Devil", "Wallaby", "Wombat", "Meerkat", "Mongoose", 
+  "Manatee", "Narwhal", "Penguin", "Flamingo", "Peacock", "Swan", 
+  "Canary", "Finch", "Sparrow", "Woodpecker", "Pigeon", "Dove", 
+  "Raven", "Crow", "Seagull", "Parakeet", "Macaw", "Cockatoo", 
+  "Budgerigar", "Quail", "Ostrich", "Emu", "Kookaburra", "Heron", 
+  "Eagle", "Hawk", "Falcon", "Vulture", "Condor", "Albatross", 
+  "Pelican", "Seal Lion", "Walrus", "Sea Lion", "Sea Turtle", 
+  "Starfish", "Jellyfish", "Sea Urchin", "Coral", "Anemone", 
+  "Lobster", "Crayfish", "Eel", "Pike", "Trout", "Salmon", 
+  "Catfish", "Tuna", "Mackerel", "Snapper", "Bass", "Grouper", 
+  "Swordfish", "Marlin", "Haddock", "Flounder", "Sardine", 
+  "Anchovy", "Mullet", "Perch", "Carp", "Goldfish", "Betta", 
+  "Guppy", "Molly", "Platies", "Cichlid", "Angelfish", "Koi", 
+  "Tetra", "Barb", "Rainbowfish", "Killifish", "Dwarf Pufferfish"
+];
 
-const mindiv = document.querySelector(".mins");
-const secdiv = document.querySelector(".secs");
+// Repeat or expand this list to reach 1000 names as needed.
 
-const startBtn = document.querySelector(".start");
-localStorage.setItem("btn", "focus");
 
-let initial, totalsecs, perc, paused, mins, seconds;
 
-startBtn.addEventListener("click", () => {
-  let btn = localStorage.getItem("btn");
+var score = 0; // Initialize score
+var generatedLetter = ''; // Variable to store the randomly generated letter
 
-  if (btn === "focus") {
-    mins = +localStorage.getItem("focusTime") || 1;
+function checkWord() {
+  const userInput = document.getElementById("userInput").value;
+  const resultDiv = document.getElementById("result"); // Get result div
+
+  if (validWords.includes(userInput)) {
+      score += 10; // Add 10 points if the word is valid
+      resultDiv.textContent = "The word is valid!"; // Display valid message
+  } else if (userInput.startsWith(generatedLetter)) {
+      score += 10; // Add 10 points if the word starts with the generated letter
+      resultDiv.textContent = "The word starts with the correct letter!"; // Display message
   } else {
-    mins = +localStorage.getItem("breakTime") || 1;
+      resultDiv.textContent = "The word is not valid."; // Display invalid message
   }
 
+  document.getElementById("score").textContent = score; // Update score display
+  document.getElementById("userInput").value = ''; // Clear input field
+}
+
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var i = 0;
+var intervalID;
+var usedLetters = new Set(); // Set to track used letters
+
+function startGenerating() {
+  clearInterval(intervalID);
+  usedLetters.clear(); // Clear the set for new session
+
+  intervalID = setInterval(function() {
+      document.getElementById("alpha").innerHTML = alphabet[i];
+      i++;
+      if (i === alphabet.length) {
+          i = 0; // Reset to start of the alphabet
+      }
+  }, 100); // Change letter every 100 ms
+
+  setTimeout(stopGenerating, 1000); // Adjust how long it loops
+}
+
+function stopGenerating() {
+  clearInterval(intervalID);
+  
+  let randomIndex = Math.floor(Math.random() * alphabet.length);
+  generatedLetter = alphabet[randomIndex]; // Store the randomly generated letter
+  document.getElementById("alpha").innerHTML = generatedLetter; // Display the letter
+}
+
+// Timer setup
+const mindiv = document.querySelector(".mins");
+const secdiv = document.querySelector(".secs");
+const bell = document.querySelector("audio");
+const startBtn = document.querySelector(".start");
+let initial, mins, seconds;
+
+startBtn.addEventListener("click", () => {
+  mins = +document.getElementById("focusTime").value || 1;
   seconds = mins * 60;
-  totalsecs = mins * 60;
-  setTimeout(decremenT(), 60);
-  startBtn.style.transform = "scale(0)";
-  paused = false;
+  startGenerating(); // Start generating letters
+  decremenT(); // Start the timer
+  startBtn.style.transform = "scale(0)"; // Hide the start button
 });
 
 function decremenT() {
   mindiv.textContent = Math.floor(seconds / 60);
   secdiv.textContent = seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`;
-  if (circle.classList.contains("danger")) {
-    circle.classList.remove("danger");
-  }
 
   if (seconds > 0) {
-    perc = Math.ceil(((totalsecs - seconds) / totalsecs) * 100);
-    setProgress(perc);
-    seconds--;
-    initial = window.setTimeout("decremenT()", 1000);
-    if (seconds < 10) {
-      circle.classList.add("danger");
-    }
+      seconds--;
+      setTimeout(decremenT, 1000);
   } else {
-    mins = 0;
-    seconds = 0;
-    bell.play();
-    let btn = localStorage.getItem("btn");
-
-    if (btn === "focus") {
-      startBtn.textContent = "start break";
-      startBtn.classList.add("break");
-      localStorage.setItem("btn", "break");
-    } else {
-      startBtn.classList.remove("break");
-      startBtn.textContent = "start focus";
-      localStorage.setItem("btn", "focus");
-    }
-    startBtn.style.transform = "scale(1)";
+      bell.play();
+      checkWord(); // Call checkWord function when timer runs out
+      startBtn.style.transform = "scale(1)"; // Show the start button again
   }
 }
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  localStorage.setItem("focusTime", document.getElementById("focusTime").value);
+});
