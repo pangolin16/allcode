a584258eba38aab9d0ce2752da0772e912ef5eaa
pangolin16 <m.tojnar@gmail.com>
2026-03-01 09:16:56 +0100

    Update UradPraceSearcher class definition

---
+++ urad_prace_search.py
@@ -0,0 +1,10 @@
+class UradPraceSearcher:
+    def __init__(self):
+        # Initialize your searcher here
+        pass
+
+    def search_jobs(self, query):
+        # Implement the logic to search for jobs here
+        pass
+
+    # Other API interaction methods can be added here
